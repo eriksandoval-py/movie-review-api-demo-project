bd97204f48aabbefce6aa9ac93cac61297950275
Erik Sandoval <eriksandoval.py@gmail.com>
2023-06-29 23:56:39 -0500

    Rename.

---
+++ src/main/java/com/eriksandoval/springbootdemoproject/MovieReviewAPIDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eriksandoval.springbootdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewAPIDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewAPIDemoProjectApplication.class, args);
+	}
+
+}
