984fdf8514a00ae9cc69eee788285921b46a1e28
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-01 19:08:58 -0500

    Initial creation of endpoint.

--- src/main/java/com/eriksandoval/moviereviewapidemoproject/MovieReviewAPIDemoProjectApplication.java
+++ src/main/java/com/eriksandoval/moviereviewapidemoproject/MovieReviewAPIDemoProjectApplication.java
@@ -2,12 +2,20 @@ package com.eriksandoval.moviereviewapidemoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MovieReviewAPIDemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieReviewAPIDemoProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World!";
 	}
 
 }
